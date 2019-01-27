a7d0bcfaaaaf4b02c36940d6168549b5cd08929a
Eric Peterson <eric@puzzlehead.org>
2019-01-27 13:00:29 -0800

    Starting the code base

---
+++ public/org/puzzlehead/sqleasy/database.hpp
@@ -0,0 +1,83 @@
+/*
+ * MIT License
+ * 
+ * Copyright (c) 2019 Eric Peterson eric@puzzlehead.org
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#ifndef ORG_PUZZLEHEAD_SQLEASY_DATABASE_HPP_
+#define ORG_PUZZLEHEAD_SQLEASY_DATABASE_HPP_
+
+#include <memory>
+#include <string>
+
+#include <sqlite3.h>
+
+namespace org
+{
+namespace puzzlehead
+{
+namespace sqleasy
+{
+
+class Database
+{
+public:
+
+	using Handle = std::shared_ptr<sqlite3>;
+
+	explicit Database(const std::string& filename);
+
+	Database(const std::string& filename, const int flags);
+
+	Database(const std::string& filename, const std::string& vfs);
+
+	Database(const std::string& filename, const int flags, const std::string& vfs);
+
+	virtual ~Database() = default;
+
+	explicit operator bool();
+
+	Handle handle();
+
+	int exec(const std::string& sql);
+
+	int errorCode();
+
+	int extendedErrorCode();
+
+	std::string errorMessage();
+
+	static std::string errorString(const int errorCode);
+
+protected:
+
+	static constexpr int DEFAULT_FLAGS = SQLITE_OPEN_CREATE | SQLITE_OPEN_READWRITE;
+
+	static constexpr auto DEFAULT_VFS = "";
+
+	Handle m_handle = nullptr;
+};
+
+} /* namespace sqleasy */
+} /* namespace puzzlehead */
+} /* namespace org */
+
+#endif /* ORG_PUZZLEHEAD_SQLEASY_DATABASE_HPP_ */
---
+++ src/database.cpp
@@ -0,0 +1,99 @@
+/*
+ * MIT License
+ * 
+ * Copyright (c) 2019 Eric Peterson eric@puzzlehead.org
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#include <org/puzzlehead/sqleasy/database.hpp>
+
+namespace org
+{
+namespace puzzlehead
+{
+namespace sqleasy
+{
+
+Database::Database(const std::string& filename) :
+		Database(filename, DEFAULT_FLAGS, DEFAULT_VFS)
+{
+}
+
+Database::Database(const std::string& filename, const int flags) :
+		Database(filename, flags, DEFAULT_VFS)
+{
+}
+
+Database::Database(const std::string& filename, const std::string& vfs) :
+		Database(filename, DEFAULT_FLAGS, vfs)
+{
+}
+
+Database::Database(const std::string& filename, const int flags,
+		const std::string& vfs)
+{
+	sqlite3 * db = nullptr;
+
+	if (sqlite3_open_v2(filename.c_str(), &db, flags,
+			vfs == "" ? nullptr : vfs.c_str()) == SQLITE_OK)
+	{
+		m_handle.reset(db, sqlite3_close_v2);
+	}
+}
+
+Database::operator bool()
+{
+	return m_handle != nullptr;
+}
+
+Database::Handle Database::handle()
+{
+	return m_handle;
+}
+
+int Database::exec(const std::string& sql)
+{
+	return m_handle == nullptr ? SQLITE_MISUSE : sqlite3_exec(m_handle.get(), sql.c_str(), nullptr, nullptr, nullptr);
+}
+
+int Database::errorCode()
+{
+	return m_handle == nullptr ? SQLITE_MISUSE : sqlite3_errcode(m_handle.get());
+}
+
+int Database::extendedErrorCode()
+{
+	return m_handle == nullptr ? SQLITE_MISUSE : sqlite3_errcode(m_handle.get());
+}
+
+std::string Database::errorMessage()
+{
+	return m_handle == nullptr ? "" : sqlite3_errmsg(m_handle.get());
+}
+
+std::string Database::errorString(const int errorCode)
+{
+	return sqlite3_errstr(errorCode);
+}
+
+}
+/* namespace sqleasy */
+} /* namespace puzzlehead */
+} /* namespace org */
